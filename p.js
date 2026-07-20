0ea4fa4d09687990d55e691c15030f9f1e17260f
ShivamiNautiyal <shivaminautiyal77@gmail.com>
2026-07-20 14:33:18 +0530

    Add AI voice introduction feature

--- p.js
+++ p.js
@@ -46,3 +46,118 @@ form.addEventListener("submit", async function (e) {
     status.style.color = "red";
   }
 });
+const toggle = document.getElementById("theme-toggle");
+
+// Load saved theme
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+    toggle.checked = true;
+}
+
+toggle.addEventListener("change",()=>{
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+
+        localStorage.setItem("theme","dark");
+
+    }else{
+
+        localStorage.setItem("theme","light");
+
+    }
+
+});
+
+const voiceBtn = document.getElementById("voice-btn");
+
+
+// Introduction text
+const introduction = `
+Hello, I am Shivami Nautiyal.
+
+I am a BTech Information Technology student
+and a frontend developer.
+
+I have skills in HTML, CSS, JavaScript, React and SQL.
+
+I have a passion for web development and I enjoy building user-friendly and visually appealing websites.
+
+I have built projects like Facebook Clone,
+ and my Personal Portfolio Website. these projects showcase my skills in frontend development and my ability to create responsive and interactive web applications.
+
+I love creating responsive and interactive websites
+and I am continuously improving my development skills.
+
+Thank you for visiting my portfolio.
+`;
+
+
+// Function to speak
+function speakIntroduction() {
+
+    let speech = new SpeechSynthesisUtterance(introduction);
+
+
+    // Get all available voices
+    let voices = window.speechSynthesis.getVoices();
+
+
+    // Find female voice
+    let femaleVoice = voices.find(voice => 
+
+        voice.name.toLowerCase().includes("female") ||
+
+        voice.name.toLowerCase().includes("zira") 
+
+        
+    );
+
+
+    // If female voice found, apply it
+    if(femaleVoice){
+
+        speech.voice = femaleVoice;
+
+    }
+
+
+    speech.lang = "en-US";
+
+    speech.rate = 0.9;
+
+    speech.pitch = 1.3;
+
+    speech.volume = 1;
+
+
+    window.speechSynthesis.cancel();
+
+    window.speechSynthesis.speak(speech);
+
+}
+
+
+// Button click
+voiceBtn.addEventListener("click", () => {
+
+    speakIntroduction();
+
+});
+
+
+// Load voices properly
+window.speechSynthesis.onvoiceschanged = () => {
+
+    let voices = window.speechSynthesis.getVoices();
+
+    console.log("Available Voices:");
+
+    voices.forEach(voice => {
+
+        console.log(voice.name);
+
+    });
+
+};
